975739e3d3c04364bb35c72a3c2bc0a8c2904c4a
Stipe Kovačić <skovacic1@foi.hr>
2020-11-18 23:25:37 +0100

    AIR2025-3 Initial commit - API setup - Created products controller that recieves an image

---
+++ WebService/Wienerberger.WebService/Wienerberger.WebService.IoC.Configuration/DI/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Wienerberger.WebService.Services;
+using Wienerberger.WebService.Services.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Wienerberger.WebService.Services.Services;
+
+namespace Wienerberger.WebService.IoC.Configuration.DI
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigureBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services != null)
+            {
+                services.AddSingleton<IImageService, ImageService>();
+            }
+        }
+
+        public static void ConfigureMappings(this IServiceCollection services)
+        {
+            if (services != null)
+            {
+                //Automap settings
+                services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            }
+        }
+    }
+}
