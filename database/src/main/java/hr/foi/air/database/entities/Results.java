69df96561ffaa98158c34bbc2f05b628b1db7828
dragan <drazovko@foi.hr>
2021-01-02 18:14:09 +0100

    Database created
    Added DAO interface with insert, update, delete and query definitions
    Used singleton in MyDatabase.java to make room database accessible
    In Results.java puts getters and setters

--- database/src/main/java/hr/foi/air/database/entities/Results.java
+++ database/src/main/java/hr/foi/air/database/entities/Results.java
@@ -5,10 +5,26 @@ import androidx.room.ForeignKey;
 
 @Entity(primaryKeys = {"idPicture", "idProduct"})
 public class Results {
-            @ForeignKey(
-                    entity = Picture.class, parentColumns = "id", childColumns = "idPicture"
-            )
+    @ForeignKey(
+            entity = Picture.class, parentColumns = "id", childColumns = "idPicture"
+    )
     int idPicture;
-            @ForeignKey(entity = Product.class, parentColumns = "id", childColumns = "idProduct")
+    @ForeignKey(entity = Product.class, parentColumns = "id", childColumns = "idProduct")
     int idProduct;
+
+    public int getIdPicture() {
+        return idPicture;
+    }
+
+    public void setIdPicture(int idPicture) {
+        this.idPicture = idPicture;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
 }
